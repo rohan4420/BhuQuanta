cf65bcf09242fb29adaa5709bea9b54d77649c21
Arpit Singh <79187271+Arpit08-coder-glitch@users.noreply.github.com>
2024-06-13 17:16:58 +0530

    Update ResultPage.js

--- client/screens/ResultPage.js
+++ client/screens/ResultPage.js
@@ -1,29 +1,34 @@
-// screens/ResultScreen.js
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import Navbar from "./NavBar";
+import { WebView } from 'react-native-webview';
 
 const ResultScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
-  // const { district, tehsil, village, khasra } = route.params;
+  const { state, district, tehsil, village, khasra } = route.params;
+
+    // Constructing the HTML file path
+    const htmlPath = require('../assets/Map.html'); // Adjust the path accordingly
+
+    // Inject JavaScript to initialize the map with parameters
+    const injectedJavaScript = `
+        initializeMap('${state}', '${district}', '${tehsil}', '${village}', '${khasra}');
+    `;
 
   return (
     <View style={styles.container}>
       <Navbar />
-      <Text style={styles.title}>Result Screen</Text>
-      <Text style={styles.resultText}>State:Maharashtra</Text>
-      <Text style={styles.resultText}>District: Solapur</Text>
-      <Text style={styles.resultText}>Tehsil: Jule Solapur</Text>
-      <Text style={styles.resultText}>Village: Borgaon</Text>
-      <Text style={styles.resultText}>Khasra: 143</Text>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("Home")}
-      >
-        <Text style={styles.buttonText}>Back to Home</Text>
+      <WebView
+        style={styles.webview}
+        originWhitelist={['*']}
+        source={htmlPath} // Pass the HTML file path
+        javaScriptEnabled={true} // Enable JavaScript execution in the WebView
+        injectedJavaScript={injectedJavaScript}
+      />
+      <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+        <Text style={styles.buttonText}>Go Back</Text>
       </TouchableOpacity>
     </View>
   );
@@ -33,7 +38,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: "center",
     backgroundColor: "#f8f9fa",
   },
   title: {
@@ -47,6 +51,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 10,
     color: "#495057",
+  },
+  webview: {
+    flex: 1,
+    marginTop: 20,
   },
   button: {
     backgroundColor: "#007bff",
